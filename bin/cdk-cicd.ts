31b1505b5b80a67ad5172d9712822acd58481737
Demetrious Wilson <demetriouswilson@gmail.com>
2025-10-23 21:21:12 -0700

    Add app.synth to CodePipeline

--- bin/cdk-cicd.ts
+++ bin/cdk-cicd.ts
@@ -5,3 +5,6 @@ import { CdkCicdStack } from '../lib/cdk-cicd-stack';
 const app = new cdk.App();
 new CdkCicdStack(app, 'CdkCicdStack', {
 });
+
+// Without this the pipeline won't work
+app.synth();
