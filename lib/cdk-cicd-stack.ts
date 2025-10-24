0d1bbab5ab48c1ca2c66139ee3f5d85363b0ec65
Demetrious Wilson <demetriouswilson@gmail.com>
2025-10-23 21:08:58 -0700

    Update the CodePipeline

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,8 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 
 export class CdkCicdStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-  }
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        new CodePipeline(this, 'AwesomePipeline', {
+            pipelineName: 'AwesomePipeline',
+            synth: new ShellStep('Synth', {
+                input: CodePipelineSource.gitHub('taywils/cdk-course-resources', 'main'),
+                commands: [
+                    'npm ci',
+                    'npx cdk synth'
+                ],
+                primaryOutputDirectory: 'cdk.out'
+            })
+        })
+    }
 }
