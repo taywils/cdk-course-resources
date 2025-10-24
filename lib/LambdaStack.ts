5dfcbfe2ddd08fbb9e26e71d701b6544fb8ee25f
Demetrious Wilson <demetriouswilson@gmail.com>
2025-10-23 21:47:56 -0700

    Add services directory Add new handler Lambda function to LambdaStack

--- lib/LambdaStack.ts
+++ lib/LambdaStack.ts
@@ -1,5 +1,8 @@
 import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
+import { join } from 'path'
 
 interface LambdaStackProps extends StackProps {
     stageName?: string
@@ -8,5 +11,14 @@ interface LambdaStackProps extends StackProps {
 export class LambdaStack extends Stack {
     constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props);
+
+        new NodejsFunction(this, 'hello-lambda-nodejs', {
+            runtime: Runtime.NODEJS_LATEST,
+            handler: 'handler',
+            entry: (join(__dirname, '..', 'services', 'hello.ts')),
+            environment: {
+                STAGE: props.stageName!
+            }
+        })
     }
 }
